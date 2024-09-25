fae13a470a41d03b9c4d9e25e42499389105ee40
Danver <danver.ver@hotmail.com>
2024-09-25 18:01:08 -0300

    Adição do checkup de cache

--- src/components/api/getFullData.js
+++ src/components/api/getFullData.js
@@ -1,124 +1,113 @@
-/*
-
-// 1.
-Aplicar o chache para a função doRequest
-recomendo essa lib
-https://www.npmjs.com/package/node-cache
-Para gravar :
-  myCache.set( "URL-DA-API", retornoDaApi, 10000 );
-Para ler:
-  myCache.get( "URL-DA-API" );
-Testar com logs para ver se esta pegando do cache ou do fetch
-
-// 2.
-carregar as evoluções de cada  pokemom 
-com a funcao getSmallerInfoByName 
-
-// 3.
-rever as informações do `getFullData.to_return` 
-para ver o que voce usa e o que não usa 
-e adicionar apenas o que for usar.
-se for usar outra chamada de api, criar uma função para ela.
-*/
-
+import NodeCache from 'node-cache'
 const baseURL = `https://pokeapi.co/api/v2`;
 
+
 const doRequest = async (url) => {
-  // Se URL estiver no cache, retorno o cache
-  // Se não, faço o fetch
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+    const myCache = new NodeCache();
+
+    if (myCache.get(url)) {
+        return myCache.get(url)
     }
-    // Salvar a resposta no cache
-    return await response.json();
-  } catch (error) {
-    console.log(error.message);
-    return null;
-  }
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        myCache.set(url, data, 10000)
+
+        return data
+    } catch (error) {
+        console.log(error.message);
+        return null;
+    }
 };
 
 const getFullData = async function (id) {
-  const baseInfo = await doRequest(`${baseURL}/pokemon/${id}`);
-  if (!baseInfo) {
-    return null;
-  }
+    const baseInfo = await doRequest(`${baseURL}/pokemon/${id}`);
+    if (!baseInfo) {
+        return null;
+    }
 
-  const to_return = {
-    id: baseInfo.id,
-    name: baseInfo.name,
-    abilities: await getAbility(baseInfo),
-    moves: baseInfo.moves.map((move) => move.move.name),
-    types: baseInfo.types.map((type) => type.type.name),
-    pixelImage: baseInfo.sprites.front_default,
-    animeImage: baseInfo.sprites.other["official-artwork"].front_default,
-    animeShinyImage: baseInfo.sprites.other["official-artwork"].front_shiny,
-    evolutions: await getEvolutions(baseInfo),
-  };
-  myLog(to_return)
+    const to_return = {
+        id: baseInfo.id,
+        name: baseInfo.name,
+        abilities: await getAbility(baseInfo),
+        moves: baseInfo.moves.map((move) => move.move.name),
+        types: baseInfo.types.map((type) => type.type.name),
+        pixelImage: baseInfo.sprites.front_default,
+        animeImage: baseInfo.sprites.other["official-artwork"].front_default,
+        animeShinyImage: baseInfo.sprites.other["official-artwork"].front_shiny,
+        evolutions: await getEvolutions(baseInfo),
+    };
+    myLog(to_return.evolutions)
 };
 
 const getAbility = async (baseInfo) => {
-  const abilities = await Promise.all(
-    baseInfo.abilities.map(
-      async (ability) =>
-        await doRequest(`${baseURL}/ability/${ability.ability.name}`).then(
-          (response) => {
-            return {
-              name: ability.ability.name,
-              description: response.effect_entries[1].effect,
-            };
-          },
+    const abilities = await Promise.all(
+        baseInfo.abilities.map(
+            async (ability) =>
+                await doRequest(`${baseURL}/ability/${ability.ability.name}`).then(
+                    (response) => {
+                        return {
+                            name: ability.ability.name,
+                            description: response.effect_entries[1].effect,
+                        };
+                    },
+                ),
         ),
-    ),
-  ).then((values) => {
-    return values;
-  });
-  return abilities;
+    ).then((values) => {
+        return values;
+    });
+    return abilities;
 };
 
 const getEvolutions = async (baseInfo) => {
-  const species = await doRequest(
-    `${baseURL}/pokemon-species/${baseInfo.name}`,
-  );
+    const species = await doRequest(
+        `${baseURL}/pokemon-species/${baseInfo.name}`,
+    );
 
-  let evolutionChain = {};
+    let evolutionChain = {};
 
-  if (species?.evolution_chain?.url) {
-    const evolutionChainFromApi = await doRequest(species.evolution_chain.url);
+    if (species?.evolution_chain?.url) {
+        const evolutionChainFromApi = await doRequest(species.evolution_chain.url);
+        // evolutionChain.originalSpecie = {
+        //     evolutionChainFromApi.species
+        // }
 
-    if (evolutionChain) {
-      evolutionChain.firstEvolution =
-        await Promise.all (evolutionChainFromApi.chain.evolves_to?.map(async (content) => ({
-          id: content.species.url.split("/")[6],
-          name: content.species.name,
-          imageAndTypes: await getSmallerInfoByName(content.species.url.split("/")[6]),
-          url: species.evolution_chain.url,
-          secondEvolution: await Promise.all(content.evolves_to?.map(async (content) => ({
-            id: content.species.url.split("/")[6],
-            name: content.species.name,
-            imageAndTypes: await getSmallerInfoByName(content.species.url.split("/")[6]),
-            url: species.evolution_chain.url
-          }))),
-        })));
+        if (evolutionChain) {
+            evolutionChain.firstEvolution =
+                await Promise.all(evolutionChainFromApi.chain.evolves_to?.map(async (content) => ({
+                    id: content.species.url.split("/")[6],
+                    name: content.species.name,
+                    imageAndTypes: await getSmallerInfoByName(content.species.url.split("/")[6]),
+                    url: species.evolution_chain.url,
+                    secondEvolution: await Promise.all(content.evolves_to?.map(async (content) => ({
+                        id: content.species.url.split("/")[6],
+                        name: content.species.name,
+                        imageAndTypes: await getSmallerInfoByName(content.species.url.split("/")[6]),
+                    }))),
+                })));
+        }
     }
-  }
-  return evolutionChain;
+    return evolutionChain;
 };
 
 const myLog = (data) => {
-  console.log(JSON.stringify(data, null, 4));
+    console.log(JSON.stringify(data, null, 4));
 };
 
 const getSmallerInfoByName = async (id) => {
-  const pokemon = await doRequest(`${baseURL}/pokemon/${id}`)
+    const pokemon = await doRequest(`${baseURL}/pokemon/${id}`)
 
-  return {
-    frontDefault: pokemon.sprites?.other["official-artwork"].front_default,
-    frontShinyDefault: pokemon.sprites?.other["official-artwork"].front_shiny,
-    types: pokemon.types.map((type) => type.type.name),
-  };
+    return {
+        frontDefault: pokemon.sprites?.other["official-artwork"].front_default,
+        frontShinyDefault: pokemon.sprites?.other["official-artwork"].front_shiny,
+        types: pokemon.types.map((type) => type.type.name),
+    };
 };
 
 getFullData(1)
